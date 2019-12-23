f7b117fe222752f1c3aaaabb84628bdba16beb0c
Kazumasa Shimomura <shimomura_kazumasa@cyberagent.co.jp>
2019-12-23 11:34:46 +0900

    printToday()

--- Sources/Scripts/main.swift
+++ Sources/Scripts/main.swift
@@ -1,3 +1,10 @@
 import SPMUtility
+import Foundation
 
-print("Hello, world!")
+func printToday() {
+    let dateComponents = Calendar.autoupdatingCurrent.dateComponents([.year, .month, .day], from: Date())
+    guard let year = dateComponents.year, let month = dateComponents.month, let day = dateComponents.day else { return }
+    print(String(format: "Today is %04d%02d%02d", year, month, day))
+}
+
+printToday()
